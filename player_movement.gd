d47890b4cdc3f72dde0515c64a66c3354294695b
Robert Drury <rdrury1513@gmail.com>
2024-01-07 13:40:29 +0100

    Animate running in each direction

--- player_movement.gd
+++ player_movement.gd
@@ -3,21 +3,32 @@ extends CharacterBody2D
 # Declare member variables here. Examples:
 @export var speed = 200
 @onready var animationPlayer = $sprites/AnimationPlayer
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Input handling
-	velocity = Vector2()  # Reset velocity
+	var velocity = Vector2()  # Reset velocity
 	
-	if Input.is_action_pressed("walk_down"):
-		# Set the AnimationPlayer's animation to "run_down"
+	if Input.is_action_pressed("run_up"):
+		animationPlayer.play("run_up")
+		velocity.y = -speed 
+	
+	elif Input.is_action_pressed("run_down"):
 		animationPlayer.play("run_down")
-		
-		# Adjust the velocity or perform other actions as needed for walking down
-		velocity.y += 1  # You might need to adjust this based on your game's requirements
-		
-	else:
-		# If the "walk_down" key is not pressed, stop the animation
-		animationPlayer.stop()
+		velocity.y = speed 
+
+	elif Input.is_action_pressed("run_left"):
+		animationPlayer.play("run_left")
+		velocity.x = -speed 
 	
+	elif Input.is_action_pressed("run_right"):
+		animationPlayer.play("run_right")
+		velocity.x = speed 
+
+	else:
+		velocity.x = 0
+		velocity.y = 0
+		animationPlayer.stop()
+
 	# Move the character based on the calculated velocity
 	move_and_slide()
